4517d955142dc8b59a5e637675cae31bc2c8689e
leon-bassan <>
2024-09-11 22:30:57 +0200

    Show a user's emails in an ASP.NET Core app with Microsoft Graph

--- Begin/Pages/Email.cshtml.cs
+++ Begin/Pages/Email.cshtml.cs
@@ -29,7 +29,10 @@ namespace DotNetCoreRazor_MSGraph.Pages
 
         public async Task OnGetAsync()
         {
-            // Remove this code
+            var messagesPagingData = await _graphEmailClient.GetUserMessagesPage(NextLink);
+            Messages = messagesPagingData.Messages;
+            NextLink = messagesPagingData.NextLink;
+
             await Task.CompletedTask;
         }
     }
